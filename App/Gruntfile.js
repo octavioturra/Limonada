d6483fe9935ffca2ccdd636c916eca7ca4ef1b35
Octavio Turra <octavio.turra@gmail.com>
2013-11-27 00:34:10 -0200

    Bootstrap alterado

--- App/Gruntfile.js
+++ App/Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
             options: {
                 port: (process)?process.env.PORT:3000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: (process)?process.env.IP:'localhost',
+                hostname: (process.env.IP)?process.env.IP:'127.0.0.1',
                 livereload: 35729
             },
             livereload: {
